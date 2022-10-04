45d4b4c936e67046f19a814bae1f436ea9381b05
nlom0218 <nlom0218@naver.com>
2022-10-04 23:14:19 +0900

    Step1 메뉴 추가하기 - 메뉴 리스트에 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,40 @@
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
   // form 태그가 자동으로 전송되는 걸 막아준다.
-  document
-    .querySelector("#espresso-menu-form")
-    .addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
+  $("#espresso-menu-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
 
   // 메뉴의 이름을 입력
-  document
-    .querySelector("#espresso-menu-name")
-    .addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
-        console.log(document.getElementById("espresso-menu-name").value);
-      }
-    });
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const espressMenuName = $("#espresso-menu-name").value;
+      const menuItemTemplate = (name) => {
+        return `
+          <li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${name}</span>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+              수정
+            </button>
+            <button
+              type="button"
+              class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+              삭제
+            </button>
+          </li>
+        `;
+      };
+      $("#espresso-menu-list").insertAdjacentHTML(
+        "beforeend",
+        menuItemTemplate(espressMenuName)
+      );
+    }
+  });
 }
 
 App();
