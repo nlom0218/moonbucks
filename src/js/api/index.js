6db966b985a92be79b2dc49562ab4b626ca794f8
nlom0218 <nlom0218@naver.com>
2022-10-08 20:09:55 +0900

    Step3. 리팩터링 - API 호출 객체 추상화

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,54 +1,76 @@
 // 기본 주소를 설정하여 이후에 주소가 바뀌더라도 수정이 편할 수 있도록 한다.
 const BASE_URL = "http://localhost:3000/api";
 
-export default {
-  getAllMenuByCategory: async (category) => {
-    const res = await fetch(`${BASE_URL}/category/${category}/menu`);
-    return res.json();
-  },
-
-  creaetMenu: async (category, name) => {
-    const res = await fetch(`${BASE_URL}/category/${category}/menu`, {
+const HTTP_METHOD = {
+  POST: (data) => {
+    return {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name }),
-    });
-    if (!res.ok) {
-      console.error("에러가 발생했습니다.");
-    }
+      body: JSON.stringify(data),
+    };
   },
-
-  updateMenu: async (category, menuId, name) => {
-    const res = await fetch(`${BASE_URL}/category/${category}/menu/${menuId}`, {
+  PUT: (data) => {
+    return {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name }),
-    });
-    if (!res.ok) {
-      console.error("에러가 발생했습니다.");
-    }
-    return res.json();
+      body: data ? JSON.stringify(data) : null,
+    };
   },
-
-  toggleSoldOutMenu: async (category, menuId) => {
-    const res = await fetch(
-      `${BASE_URL}/category/${category}/menu/${menuId}/soldout`,
-      {
-        method: "PUT",
-      }
-    );
-    if (!res.ok) {
-      console.error("에러가 발생했습니다.");
-    }
-    return res.json();
-  },
-
-  deleteMenu: async (category, menuId) => {
-    const res = await fetch(`${BASE_URL}/category/${category}/menu/${menuId}`, {
+  DELETE: () => {
+    return {
       method: "DELETE",
-    });
-    if (!res.ok) {
-      console.error("에러가 발생했습니다.");
-    }
+    };
+  },
+};
+
+const req = async (url, option) => {
+  const res = await fetch(url, option);
+  if (!res.ok) {
+    window.alert("에러가 발생했습니다.");
+    console.error("에러가 발생했습니다.");
+  }
+  return res.json();
+};
+
+const reqWithOutJson = async (url, option) => {
+  const res = await fetch(url, option);
+  if (!res.ok) {
+    window.alert("에러가 발생했습니다.");
+    console.error("에러가 발생했습니다.");
+  }
+  return res;
+};
+
+export default {
+  getAllMenuByCategory: (category) => {
+    return req(`${BASE_URL}/category/${category}/menu`);
+  },
+
+  creaetMenu: (category, name) => {
+    return req(
+      `${BASE_URL}/category/${category}/menu`,
+      HTTP_METHOD.POST({ name })
+    );
+  },
+
+  updateMenu: (category, menuId, name) => {
+    return req(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      HTTP_METHOD.PUT({ name })
+    );
+  },
+
+  toggleSoldOutMenu: (category, menuId) => {
+    return req(
+      `${BASE_URL}/category/${category}/menu/${menuId}/soldout`,
+      HTTP_METHOD.PUT()
+    );
+  },
+
+  deleteMenu: (category, menuId) => {
+    reqWithOutJson(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      HTTP_METHOD.DELETE()
+    );
   },
 };
